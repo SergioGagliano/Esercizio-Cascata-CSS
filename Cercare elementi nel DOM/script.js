3d8ca1c3e30c57c6d239162934f2394579b307cf
SergioGagliano <154426562+SergioGagliano@users.noreply.github.com>
2024-02-27 21:39:34 +0100

    Add files via upload

---
+++ Cercare elementi nel DOM/script.js
@@ -0,0 +1,5 @@
+const paragrafi= document.querySelectorAll('.paragrafo');
+const elemento_tre= document.getElementById('elemento-3');
+const titoli_h2= document.querySelectorAll('h2')
+
+console.group(paragrafi,elemento_tre,titoli_h2)
