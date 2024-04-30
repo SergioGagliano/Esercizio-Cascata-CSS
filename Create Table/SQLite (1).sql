e8b3e004a6737abaf9604f366b7829da9b9a64fb
Sergio Gagliano <154426562+SergioGagliano@users.noreply.github.com>
2024-04-30 13:49:35 +0200

    Add files via upload

---
+++ Create Table/SQLite (1).sql
@@ -0,0 +1,14 @@
+CREATE TABLE Libri (
+  book_id INTEGER PRIMARY KEY,
+  titolo TEXT,
+  autore TEXT,
+  gender TEXT VARCHAR(1),
+  puplicato_anno INT,
+  isbn INT,
+  prezzo INT,
+  valutazione INT,
+  stock_count INT
+)
+  
+  
+  
